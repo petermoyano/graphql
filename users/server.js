8ad10c1cc220cee0d71d738ebf884dc22acb8b73
Peter M <pmoyano@solvd.com>
2023-05-09 11:15:30 -0300

    Add schema file

--- users/server.js
+++ users/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql"); //Similar to SQLAlchemy
+const schema = require("./schema/schema");
+
 const app = express();
 
 app.use(
     "/graphql",
     graphqlHTTP({
+        schema,
         graphiql: true,
     })
 );
